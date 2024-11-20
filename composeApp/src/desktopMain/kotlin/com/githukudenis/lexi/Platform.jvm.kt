4c9c93fefc374cbff915735ab6b6d4531dcc3651
githukudenis <githukudenis@gmail.com>
2024-11-20 07:45:10 +0300

    Initial commit

---
+++ composeApp/src/desktopMain/kotlin/com/githukudenis/lexi/Platform.jvm.kt
@@ -0,0 +1,7 @@
+package com.githukudenis.lexi
+
+class JVMPlatform: Platform {
+    override val name: String = "Java ${System.getProperty("java.version")}"
+}
+
+actual fun getPlatform(): Platform = JVMPlatform()
